170ea769d5dc7ee1d51efb12e155800b918c0a1c
Mahfuz-Hasan <mahfuzhasan587@gmail.com>
2023-04-08 12:38:24 +0600

    cart add and remove added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Cart = ({cart}) => {
+const Cart = ({cart,handleRemoveFromCart}) => {
     return (
         <div>
             <p className='px-10 py-2 bg- shadow-lg w-fit mx-auto rounded-lg my-5 bg-stone-100'>Order Summery</p>
@@ -8,10 +8,10 @@ const Cart = ({cart}) => {
            <hr /><br />
             
             {
-                cart.map(tshirt => <div className='flex text-center justify-between items-center px-5'>
-                    <p className=' w-32 px-4 rounded-md border-2 text-center' key={tshirt._id}>{tshirt.name}
+                cart.map(tshirt => <div key={tshirt._id} className='flex text-center justify-between items-center px-5'>
+                    <p className=' w-32 px-4 rounded-md border-2 text-center' >{tshirt.name}
                     </p>
-                    <p className=''><button className='hover:bg-amber-500 text-start  px-4 rounded-md border-2 my-2'>X</button></p>
+                    <p><button className='hover:bg-amber-500 text-start  px-4 rounded-md border-2 my-2' onClick={(()=> handleRemoveFromCart(tshirt._id))}>X</button></p>
                 </div>)
             }
             
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,6 +10,11 @@ const Home = () => {
         const newCart = [...cart,tshirt];
         setCart(newCart);
     }
+
+    const handleRemoveFromCart = id =>{
+        const remaining = cart.filter(ts => ts._id !==id);
+        setCart(remaining);
+    }
     return (
         <div className='home-container flex w-11/12 mx-auto justify-center gap-10 gap-36 drop-shadow-md rounded-md my-10 w-fit'>
             <div className="tshirts-container grid grid-cols-3 gap-10 w-fit">
@@ -22,7 +27,9 @@ const Home = () => {
             }
             </div>
             <div className="cart-container w-64 h-fit text-center rounded-lg border-2 drop-shadow-lg pb-10">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}
+                handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
             </div>
         </div>
     );
