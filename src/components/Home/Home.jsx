30dc1610b1bdcdeac19b711da838c03e9b791498
Mahfuz-Hasan <mahfuzhasan587@gmail.com>
2023-04-08 13:16:47 +0600

    toast added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,38 +1,55 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Tshirt from '../Tshirt/Tshirt';
-import Cart from '../Cart/Cart';
-
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import Tshirt from "../Tshirt/Tshirt";
+import Cart from "../Cart/Cart";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Home = () => {
-    const tshirts = useLoaderData();
-    const [cart,setCart] = useState([]);
-    const handleAddTocart = tshirt =>{
-        const newCart = [...cart,tshirt];
-        setCart(newCart);
-    }
+  const tshirts = useLoaderData();
+  const [cart, setCart] = useState([]);
 
-    const handleRemoveFromCart = id =>{
-        const remaining = cart.filter(ts => ts._id !==id);
-        setCart(remaining);
+  const handleAddTocart = (tshirt) => {
+    const exist = cart.find((ts) => ts._id === tshirt._id);
+    if (exist) {
+      toast("Wow so easy!");
+    } else {
+      const newCart = [...cart, tshirt];
+      setCart(newCart);
     }
-    return (
-        <div className='home-container flex w-11/12 mx-auto justify-center gap-10 gap-36 drop-shadow-md rounded-md my-10 w-fit'>
-            <div className="tshirts-container grid grid-cols-3 gap-10 w-fit">
-            {
-                tshirts.map(tshirt => <Tshirt
-                key={tshirt._id}
-                tshirt = {tshirt}
-                handleAddTocart = {handleAddTocart}
-                ></Tshirt>)
-            }
-            </div>
-            <div className="cart-container w-64 h-fit text-center rounded-lg border-2 drop-shadow-lg pb-10">
-                <Cart cart={cart}
-                handleRemoveFromCart={handleRemoveFromCart}
-                ></Cart>
-            </div>
-        </div>
-    );
+  };
+
+  const handleRemoveFromCart = (id) => {
+    const remaining = cart.filter((ts) => ts._id !== id);
+    setCart(remaining);
+  };
+  return (
+    <div className="home-container flex w-11/12 mx-auto justify-center gap-10 gap-36 drop-shadow-md rounded-md my-10 w-fit">
+      <div className="tshirts-container grid grid-cols-3 gap-10 w-fit">
+        {tshirts.map((tshirt) => (
+          <Tshirt
+            key={tshirt._id}
+            tshirt={tshirt}
+            handleAddTocart={handleAddTocart}
+          ></Tshirt>
+        ))}
+      </div>
+      <div className="cart-container w-64 h-fit text-center rounded-lg border-2 drop-shadow-lg pb-10">
+        <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
+      </div>
+      <ToastContainer
+       position="top-right"
+       autoClose={5000}
+       hideProgressBar={false}
+       newestOnTop={false}
+       closeOnClick
+       rtl={false}
+       pauseOnFocusLoss
+       draggable
+       pauseOnHover
+       theme="light"
+      />
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Home from '../Home/Home';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
-
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>  
+    
         </div>
     );
 };
